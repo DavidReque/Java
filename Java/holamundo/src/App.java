d0646084e61a44ae6e0da41e92258318b49fc05d
DavidReque <108202767+DavidReque@users.noreply.github.com>
2022-09-15 19:44:16 -0600

    Add files via upload

---
+++ Java/holamundo/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(10, 20, 20);
+
+        System.out.println(resultado);
+    }
+
+
+public static int suma(int a, int b, int c) {
+    return a + b + c;
+
+}
+
+}
